e60a57b30c3ff466865c49efa1d5343068334a45
Jean Carlo Flores Carrasco <jeancarlo_flores93@hotmail.com>
2020-01-06 12:26:00 -0500

    modified port

--- app/application.go
+++ app/application.go
@@ -12,6 +12,6 @@ var (
 func StartApplication() {
 	mapUrls()
 	logger.Info("about to start the application...")
-	router.Run(":8080")
+	router.Run(":8082")
 	logger.Log.Info("about to finish the application...")
 }
